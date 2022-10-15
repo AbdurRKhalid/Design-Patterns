4834558b9acc3d5ad617e324a8b726792702ee41
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:16:17 +0200

    Added the Properties for the Manual of Car

---
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/ConcreteBuilder/CarManualBuilder.java
@@ -0,0 +1,10 @@
+package ConcreteBuilder;
+
+public class CarManualBuilder {
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmiossion transmiossion;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+}
