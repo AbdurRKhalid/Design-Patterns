411e0862e9e7e77301aba553190699a9da1bb9d5
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:40:27 +0200

    Added the Methods to get Different Objects for Each Car Type

--- Builder/Java Implementation/BuilderDesignPattern/src/main/java/Director/Director.java
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/Director/Director.java
@@ -1,4 +1,32 @@
 package Director;
 
+import Builder.Builder;
+import Components.*;
+
 public class Director {
+    public void constructSportsCar(Builder builder) {
+        builder.setCarType(CarType.SPORTS_CAR);
+        builder.setSeats(2);
+        builder.setEngine(new Engine(3.0, 0));
+        builder.setTransmission(Transmission.SEMI_AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGPSNavigator(new GPSNavigator());
+    }
+
+    public void constructCityCar(Builder builder) {
+        builder.setCarType(CarType.CITY_CAR);
+        builder.setSeats(2);
+        builder.setEngine(new Engine(1.2, 0));
+        builder.setTransmission(Transmission.AUTOMATIC);
+        builder.setTripComputer(new TripComputer());
+        builder.setGPSNavigator(new GPSNavigator());
+    }
+
+    public void constructSUV(Builder builder) {
+        builder.setCarType(CarType.SUV);
+        builder.setSeats(4);
+        builder.setEngine(new Engine(2.5, 0));
+        builder.setTransmission(Transmission.MANUAL);
+        builder.setGPSNavigator(new GPSNavigator());
+    }
 }
