741d2812f38652014928a06720f3981e8484ce7a
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:31:35 +0200

    Added the Implementation of the Trip Computer Component

---
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/Components/TripComputer.java
@@ -0,0 +1,21 @@
+package Components;
+
+public class TripComputer {
+    private Car car;
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void showFuelLevel() {
+        System.out.println("Fuel level: " + car.getFuel());
+    }
+
+    public void showStatus() {
+        if (this.car.getEngine().isStarted()) {
+            System.out.println("Car is started");
+        } else {
+            System.out.println("Car isn't started");
+        }
+    }
+}
