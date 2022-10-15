0e5eae197ce0677b7a854d9cc652015aaba62a88
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:09:06 +0200

    Added the Builder Interface with the Methods Declaration

---
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/Builder/Builder.java
@@ -0,0 +1,10 @@
+package Builder;
+
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seat);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigatior gpsNavigator);
+}
