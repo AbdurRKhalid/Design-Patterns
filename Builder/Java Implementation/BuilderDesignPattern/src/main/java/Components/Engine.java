e318b56d146e704688bcf214c31aa0167b250473
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:29:00 +0200

    Added the Implementation of the Engine Component Class

---
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/Components/Engine.java
@@ -0,0 +1,40 @@
+package Components;
+
+public class Engine {
+    private final double volume;
+    private double mileage;
+    private boolean started;
+
+    public Engine(double volume, double mileage) {
+        this.volume = volume;
+        this.mileage = mileage;
+    }
+
+    public void on() {
+        started = true;
+    }
+
+    public void off() {
+        started = false;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void go(double mileage) {
+        if (started) {
+            this.mileage += mileage;
+        } else {
+            System.err.println("Cannot go(), you must start engine first!");
+        }
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+}
