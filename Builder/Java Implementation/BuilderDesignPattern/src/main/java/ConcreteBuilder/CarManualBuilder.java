318e63022663724445129daa2f264e9b173d6c92
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-15 20:16:51 +0200

    Added the Setters for the Object Creation of the Manual Class

--- Builder/Java Implementation/BuilderDesignPattern/src/main/java/ConcreteBuilder/CarManualBuilder.java
+++ Builder/Java Implementation/BuilderDesignPattern/src/main/java/ConcreteBuilder/CarManualBuilder.java
@@ -7,4 +7,34 @@ public class CarManualBuilder {
     private Transmiossion transmiossion;
     private TripComputer tripComputer;
     private GPSNavigator gpsNavigator;
+
+    @Override
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seat) {
+        this.seats = seat;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmiossion = transmiossion;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
 }
