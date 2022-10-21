d3ab9f11375d646dae9bc8a8513b544d7349d795
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-21 11:15:23 +0200

    Added the Implemenation of the Advanced Remote Class.

---
+++ Structural/Bridge/Java Implementation/BridgeDesignPattern/src/main/java/AdvancedRemote.java
@@ -0,0 +1,13 @@
+import Abstraction.Device;
+import Bridge.BasicRemote;
+
+public class AdvancedRemote extends BasicRemote {
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
