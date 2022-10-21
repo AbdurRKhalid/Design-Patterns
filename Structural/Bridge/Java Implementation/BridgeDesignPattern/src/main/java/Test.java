0d7a215f1e37facc54c96e1002d85c04f9512ad8
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-21 11:21:28 +0200

    Added the Implemenation of the Client Code for the Testing Purpose.

---
+++ Structural/Bridge/Java Implementation/BridgeDesignPattern/src/main/java/Test.java
@@ -0,0 +1,20 @@
+import Abstraction.Device;
+import Bridge.BasicRemote;
+
+public class Test {
+    public static void main(String[] args) {
+        testDevice(new TV());
+    }
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
