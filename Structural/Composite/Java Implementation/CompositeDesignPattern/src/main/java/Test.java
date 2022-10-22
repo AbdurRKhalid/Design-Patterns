ccc2b2f7d8a78a682461bd2d3dd86e461b616537
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-22 17:28:20 +0200

    Added the Testing Class for the Understanding and Testing.

---
+++ Structural/Composite/Java Implementation/CompositeDesignPattern/src/main/java/Test.java
@@ -0,0 +1,29 @@
+import Client.ImageEditor;
+import Individual.*;
+import Composite.*;
+import Individual.Rectangle;
+
+import java.awt.*;
+
+public class Test {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
