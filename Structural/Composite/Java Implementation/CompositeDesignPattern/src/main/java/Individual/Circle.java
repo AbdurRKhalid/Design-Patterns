4fb4720679ac8c3bb6b9752ba8e991946232ef35
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-22 17:15:42 +0200

    Added the Implementation of the Circle Class.

--- Structural/Composite/Java Implementation/CompositeDesignPattern/src/main/java/Individual/Circle.java
+++ Structural/Composite/Java Implementation/CompositeDesignPattern/src/main/java/Individual/Circle.java
@@ -1,4 +1,27 @@
 package Individual;
 
-public class Circle {
+import java.awt.*;
+
+public class Circle extends BaseShape{
+    public int radius;
+
+    public Circle(int x, int y, int radius, Color color) {
+        super(x, y, color);
+        this.radius = radius;
+    }
+
+    @Override
+    public int getWidth() {
+        return radius * 2;
+    }
+
+    @Override
+    public int getHeight() {
+        return radius * 2;
+    }
+
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawOval(x, y, getWidth() - 1, getHeight() - 1);
+    }
 }
