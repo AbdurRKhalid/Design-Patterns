451edfd08a604599c0140e014fae50c7333f12a9
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-23 12:31:18 +0200

    Added the Implementation of the Data Source Decorator as Decorator.

---
+++ Structural/Decorator/Java Implementation/DecoratorDesignPattern/src/main/java/Decorator/DataSourceDecorator.java
@@ -0,0 +1,21 @@
+package Decorator;
+
+import Component.DataSource;
+
+public class DataSourceDecorator implements DataSource {
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
