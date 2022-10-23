6b3c7c988cec95261c3d4c9bcd7483bed32fed01
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-23 12:36:32 +0200

    Added the Testing Code for the Basic Decorator Design Pattern Implementation.

---
+++ Structural/Decorator/Java Implementation/DecoratorDesignPattern/src/main/java/Test.java
@@ -0,0 +1,23 @@
+import Component.DataSource;
+import ConcreteComponent.FileDataSource;
+import ConcreteDecorator.CompressionDecorator;
+import ConcreteDecorator.EncryptionDecorator;
+import Decorator.DataSourceDecorator;
+
+public class Test {
+    public static void main(String[] args) {
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                new EncryptionDecorator(
+                        new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
+    }
+}
