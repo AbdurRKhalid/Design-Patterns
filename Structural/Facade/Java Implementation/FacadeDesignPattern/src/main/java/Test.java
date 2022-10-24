f86d7a048c11ca583fe40f46eed99305d1d048bb
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:20:19 +0200

    Added the File for the Testing Alongside with the testing code.

---
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/Test.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Test {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
