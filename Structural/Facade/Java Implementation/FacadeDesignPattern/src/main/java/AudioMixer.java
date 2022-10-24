b6619cbc39acc38f3506e1acb642232eb4755347
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:16:29 +0200

    Added the Class to Implement the Audio Fixing Functionality.

---
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/AudioMixer.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.println("AudioMixer: fixing audio...");
+        return new File("tmp");
+    }
+}
