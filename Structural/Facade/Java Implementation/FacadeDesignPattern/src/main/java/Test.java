6f0b444d201971b9abd919c7089862c9549390d2
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:21:06 +0200

    Changed the Example File Name.

--- Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/Test.java
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/Test.java
@@ -3,6 +3,6 @@ import java.io.File;
 public class Test {
     public static void main(String[] args) {
         VideoConversionFacade converter = new VideoConversionFacade();
-        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        File mp4Video = converter.convertVideo("example.ogg", "mp4");
     }
 }
