f62db7dc3944aa6c02c85512ae77f34c6f517c8d
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:15:31 +0200

    Added the Bitrate Methods to Read and Convert the Video File.

---
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/BitrateReader.java
@@ -0,0 +1,11 @@
+public class BitrateReader {
+    public static VideoFile read(VideoFile file, Codec codec) {
+        System.out.println("BitrateReader: reading file...");
+        return file;
+    }
+
+    public static VideoFile convert(VideoFile buffer, Codec codec) {
+        System.out.println("BitrateReader: writing file...");
+        return buffer;
+    }
+}
