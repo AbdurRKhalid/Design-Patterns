0abd2c3431bd241938ddb6334aa9e19589566153
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:11:26 +0200

    Added the Implementation of the Video File Class.

---
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/VideoFile.java
@@ -0,0 +1,17 @@
+public class VideoFile {
+    private String name;
+    private String codecType;
+
+    public VideoFile(String name) {
+        this.name = name;
+        this.codecType = name.substring(name.indexOf(".") + 1);
+    }
+
+    public String getCodecType() {
+        return codecType;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
