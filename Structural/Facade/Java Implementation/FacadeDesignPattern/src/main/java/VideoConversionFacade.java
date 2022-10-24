d63bdf65a8d8d104d988365f8844f9a593e3ba09
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-24 09:18:15 +0200

    Added the Video Conversion Facade to Make access to Subsystem Simple.

---
+++ Structural/Facade/Java Implementation/FacadeDesignPattern/src/main/java/VideoConversionFacade.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class VideoConversionFacade {
+    public File convertVideo(String fileName, String format) {
+        System.out.println("VideoConversionFacade: conversion started.");
+        VideoFile file = new VideoFile(fileName);
+        Codec sourceCodec = CodecFactory.extract(file);
+        Codec destinationCodec;
+        if (format.equals("mp4")) {
+            destinationCodec = new MPEG4CompressionCodec();
+        } else {
+            destinationCodec = new OggCompressionCodec();
+        }
+        VideoFile buffer = BitrateReader.read(file, sourceCodec);
+        VideoFile intermediateResult = BitrateReader.convert(buffer, destinationCodec);
+        File result = (new AudioMixer()).fix(intermediateResult);
+        System.out.println("VideoConversionFacade: conversion completed.");
+        return result;
+    }
+}
