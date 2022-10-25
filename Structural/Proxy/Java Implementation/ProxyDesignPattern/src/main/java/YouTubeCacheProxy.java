cdad584f8de5ba558e9408b1573021295396fbe0
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-25 11:51:47 +0200

    Added the Implementation of the Cache Proxy Class.

---
+++ Structural/Proxy/Java Implementation/ProxyDesignPattern/src/main/java/YouTubeCacheProxy.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class YouTubeCacheProxy implements ThirdPartyYouTubeLib{
+    private ThirdPartyYouTubeLib youtubeService;
+    private HashMap<String, Video> cachePopular = new HashMap<String, Video>();
+    private HashMap<String, Video> cacheAll = new HashMap<String, Video>();
+
+    public YouTubeCacheProxy() {
+        this.youtubeService = new ThirdPartyYouTubeClass();
+    }
+
+    @Override
+    public HashMap<String, Video> popularVideos() {
+        if (cachePopular.isEmpty()) {
+            cachePopular = youtubeService.popularVideos();
+        } else {
+            System.out.println("Retrieved list from cache.");
+        }
+        return cachePopular;
+    }
+
+    @Override
+    public Video getVideo(String videoId) {
+        Video video = cacheAll.get(videoId);
+        if (video == null) {
+            video = youtubeService.getVideo(videoId);
+            cacheAll.put(videoId, video);
+        } else {
+            System.out.println("Retrieved video '" + videoId + "' from cache.");
+        }
+        return video;
+    }
+
+    public void reset() {
+        cachePopular.clear();
+        cacheAll.clear();
+    }
+}
