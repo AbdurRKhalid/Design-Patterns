2e115f400b8f693f3ef053a95cf43e711f9ce571
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-25 11:49:33 +0200

    Added the Main Video Class.

---
+++ Structural/Proxy/Java Implementation/ProxyDesignPattern/src/main/java/Video.java
@@ -0,0 +1,11 @@
+public class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video.";
+    }
+}
