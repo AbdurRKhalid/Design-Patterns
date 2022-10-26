8fea4f33835aeaa832645fb15ecbb874524d74ef
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-26 10:10:54 +0200

    Added the Forest Class to Make a Forest in A Simple JFRAME.

---
+++ Structural/Flyweight/Java Implementation/FlyweightDesignPattern/src/main/java/Forest.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
