fe26dcad86db9473ab87f3b5ae0e2181e247d104
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-26 10:07:18 +0200

    Added the Tree Class to Store State of Each Tree,

---
+++ Structural/Flyweight/Java Implementation/FlyweightDesignPattern/src/main/java/Tree.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
