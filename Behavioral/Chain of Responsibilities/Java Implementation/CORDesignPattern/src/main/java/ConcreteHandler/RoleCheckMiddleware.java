47220b6fa8efa3d0808c1289a2a3bb887fb36483
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-27 17:27:11 +0200

    Added the Role Check Middleware Concrete Class.

---
+++ Behavioral/Chain of Responsibilities/Java Implementation/CORDesignPattern/src/main/java/ConcreteHandler/RoleCheckMiddleware.java
@@ -0,0 +1,14 @@
+package ConcreteHandler;
+
+import Handler.Middleware;
+
+public class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
