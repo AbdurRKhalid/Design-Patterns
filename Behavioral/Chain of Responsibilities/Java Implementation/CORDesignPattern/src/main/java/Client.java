b3bcfabd2cb4fbc03473a2aaadebba22385d3d4b
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-27 17:30:13 +0200

    Added the Implementation of the Client Code.

---
+++ Behavioral/Chain of Responsibilities/Java Implementation/CORDesignPattern/src/main/java/Client.java
@@ -0,0 +1,44 @@
+import ConcreteHandler.RoleCheckMiddleware;
+import ConcreteHandler.ThrottlingMiddleware;
+import ConcreteHandler.UserExistsMiddleware;
+import Handler.Middleware;
+import Server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same
+        // components.
+        Middleware middleware = Middleware.link(
+                new ThrottlingMiddleware(2),
+                new UserExistsMiddleware(server),
+                new RoleCheckMiddleware()
+        );
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
