45bf16ca4210fa9c79925c75f8ca39f938a84e84
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-27 17:26:08 +0200

    Added the Implementation of the User Existing Concrete Class.

---
+++ Behavioral/Chain of Responsibilities/Java Implementation/CORDesignPattern/src/main/java/ConcreteHandler/UserExistsMiddleware.java
@@ -0,0 +1,23 @@
+package ConcreteHandler;
+
+import Handler.Middleware;
+
+public class UserExistsMiddleware extends Middleware {
+    private Server server;
+
+    public UserExistsMiddleware(Server server) {
+        this.server = server;
+    }
+
+    public boolean check(String email, String password) {
+        if (!server.hasEmail(email)) {
+            System.out.println("This email is not registered!");
+            return false;
+        }
+        if (!server.isValidPassword(email, password)) {
+            System.out.println("Wrong password!");
+            return false;
+        }
+        return checkNext(email, password);
+    }
+}
