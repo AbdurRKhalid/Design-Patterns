a04fed88f86a2889844fabb4ce56bf1921730954
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-28 09:04:41 +0200

    Added the Implementation of the Command History as Receiver.

---
+++ Behavioral/Command/Java Implementation/CommandDesignPattern/src/main/java/Receiver/CommandHistory.java
@@ -0,0 +1,19 @@
+package Receiver;
+
+import Command.Command;
+
+import java.util.Stack;
+
+public class CommandHistory {
+    private Stack<Command> history = new Stack<>();
+
+    public void push(Command c) {
+        history.push(c);
+    }
+
+    public Command pop() {
+        return history.pop();
+    }
+
+    public boolean isEmpty() { return history.isEmpty(); }
+}
