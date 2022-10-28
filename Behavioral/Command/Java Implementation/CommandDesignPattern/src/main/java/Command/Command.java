d3f7e4234adec2ee5b6f46c31fa7a32096de4cfe
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-28 09:01:28 +0200

    Added the Basic Structure for the Command Class.

---
+++ Behavioral/Command/Java Implementation/CommandDesignPattern/src/main/java/Command/Command.java
@@ -0,0 +1,20 @@
+package Command;
+
+public abstract class Command {
+    public Editor editor;
+    private String backup;
+
+    Command(Editor editor) {
+        this.editor = editor;
+    }
+
+    void backup() {
+        backup = editor.textField.getText();
+    }
+
+    public void undo() {
+        editor.textField.setText(backup);
+    }
+
+    public abstract boolean execute();
+}
