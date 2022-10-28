657d5f798e40c6447b79b0064b70e5e4acdfcbc8
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-28 09:01:41 +0200

    Added the Implementation of the Concrete Command Classes.

---
+++ Behavioral/Command/Java Implementation/CommandDesignPattern/src/main/java/ConcreteCommand/CopyCommand.java
@@ -0,0 +1,15 @@
+package ConcreteCommand;
+
+import Command.Command;
+
+public class CopyCommand extends Command {
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
