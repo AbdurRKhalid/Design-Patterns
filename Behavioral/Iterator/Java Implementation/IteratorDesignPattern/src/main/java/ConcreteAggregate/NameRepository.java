e47cbbcf8a02f32d4d329b600282a6b5e9f063e1
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-29 20:01:38 +0200

    Added Implementation of the NameRepository with ConcreteIterator Implementation.

---
+++ Behavioral/Iterator/Java Implementation/IteratorDesignPattern/src/main/java/ConcreteAggregate/NameRepository.java
@@ -0,0 +1,36 @@
+package ConcreteAggregate;
+
+import Aggregate.Container;
+import Iterator.Iterator;
+
+public class NameRepository implements Container {
+    public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
