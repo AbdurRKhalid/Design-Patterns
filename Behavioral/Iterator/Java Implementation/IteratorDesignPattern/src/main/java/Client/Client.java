69dbe3d72a52c8ed78ba89f32bca88dc3ca733d8
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-10-29 20:03:14 +0200

    Added the Client Code for the Testing.

---
+++ Behavioral/Iterator/Java Implementation/IteratorDesignPattern/src/main/java/Client/Client.java
@@ -0,0 +1,15 @@
+package Client;
+
+import ConcreteAggregate.NameRepository;
+import Iterator.Iterator;
+
+public class Client {
+    public static void main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+
+        for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+            String name = (String)iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
