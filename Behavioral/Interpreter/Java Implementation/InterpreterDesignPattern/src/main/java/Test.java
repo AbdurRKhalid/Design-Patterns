e23fbb86b96c64e47e2f5aa2499213dd5b727d63
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-06 11:54:32 +0100

    Added the Testing Code.

---
+++ Behavioral/Interpreter/Java Implementation/InterpreterDesignPattern/src/main/java/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    //Rule: Robert and John are male
+    public static Expression getMaleExpression(){
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    //Rule: Julie is a married women
+    public static Expression getMarriedWomanExpression(){
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+    }
+}
