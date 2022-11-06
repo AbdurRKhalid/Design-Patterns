02fadc451923372ae2c80d20e19b2616dd38800f
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-06 11:53:39 +0100

    Added the Intepretation Implementation for the AND Class.

---
+++ Behavioral/Interpreter/Java Implementation/InterpreterDesignPattern/src/main/java/AndExpression.java
@@ -0,0 +1,14 @@
+public class AndExpression implements Expression{
+    private Expression expr1 = null;
+    private Expression expr2 = null;
+
+    public AndExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) && expr2.interpret(context);
+    }
+}
