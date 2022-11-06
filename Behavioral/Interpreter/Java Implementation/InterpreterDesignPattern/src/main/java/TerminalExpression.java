053441ff4eebf7fb4330dc8c7fbe6b06958a5800
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-06 11:51:27 +0100

    Added the Implementation of the Terminal Expression Interpretation.

---
+++ Behavioral/Interpreter/Java Implementation/InterpreterDesignPattern/src/main/java/TerminalExpression.java
@@ -0,0 +1,15 @@
+public class TerminalExpression implements Expression{
+    private String data;
+
+    public TerminalExpression(String data){
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
