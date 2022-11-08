86fa83e183038c4cbbd0359ccd47ef25edcafa1d
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-08 10:58:21 +0100

    Added the Implementation of the CareTaker Methods for taking Care of State.

--- Behavioral/Memento/Java Implementation/MementoDesignPattern/src/main/java/CareTaker/TextEditor.java
+++ Behavioral/Memento/Java Implementation/MementoDesignPattern/src/main/java/CareTaker/TextEditor.java
@@ -1,11 +1,21 @@
 package CareTaker;
 
+import Memento.TextWindowState;
 import Originator.TextWindow;
 
 public class TextEditor {
     private TextWindow textWindow;
+    private TextWindowState savedTextWindow;
 
     public TextEditor(TextWindow textWindow) {
         this.textWindow = textWindow;
+    }
+
+    public void hitSave() {
+        savedTextWindow = textWindow.save();
+    }
+
+    public void hitUndo() {
+        textWindow.restore(savedTextWindow);
     }
 }
