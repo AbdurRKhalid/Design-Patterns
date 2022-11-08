160b23f8a8da42d1ce90b612ea7c0ba37d2cd117
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-08 10:51:50 +0100

    Added the Implementation of TextWindowState as Memento.

---
+++ Behavioral/Memento/Java Implementation/MementoDesignPattern/src/main/java/Memento/TextWindowState.java
@@ -0,0 +1,13 @@
+package Memento;
+
+public class TextWindowState {
+    private String text;
+
+    public TextWindowState(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
