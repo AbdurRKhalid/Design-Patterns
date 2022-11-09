96ba92d4e658f91fc7094165d3b6e532a546dd77
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-09 09:25:01 +0100

    Added the Implementation for the Second Concrete Observer.

---
+++ Behavioral/Observer/Java Implementation/ObserverDesignPattern/src/main/java/ConcreteObserver/LogOpenListener.java
@@ -0,0 +1,18 @@
+package ConcreteObserver;
+
+import Observer.EventListener;
+
+import java.io.File;
+
+public class LogOpenListener implements EventListener {
+    private File log;
+
+    public LogOpenListener(String fileName) {
+        this.log = new File(fileName);
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log " + log + ": Someone has performed " + eventType + " operation with the following file: " + file.getName());
+    }
+}
