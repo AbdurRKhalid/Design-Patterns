35b2ba32701f4301c09ea0a8fc8fd8f452de25ca
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-09 09:19:45 +0100

    Added the Implementation of the Subject.

---
+++ Behavioral/Observer/Java Implementation/ObserverDesignPattern/src/main/java/Publisher/EventManager.java
@@ -0,0 +1,31 @@
+package Publisher;
+
+import java.io.File;
+import java.util.*;
+
+public class EventManager {
+    Map<String, List<EventListener>> listeners = new HashMap<>();
+
+    public EventManager(String... operations) {
+        for (String operation : operations) {
+            this.listeners.put(operation, new ArrayList<>());
+        }
+    }
+
+    public void subscribe(String eventType, EventListener listener) {
+        List<EventListener> users = listeners.get(eventType);
+        users.add(listener);
+    }
+
+    public void unsubscribe(String eventType, EventListener listener) {
+        List<EventListener> users = listeners.get(eventType);
+        users.remove(listener);
+    }
+
+    public void notify(String eventType, File file) {
+        List<EventListener> users = listeners.get(eventType);
+        for (EventListener listener : users) {
+            listener.update(eventType, file);
+        }
+    }
+}
