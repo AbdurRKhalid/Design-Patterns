985a1e84313cdbff9ea8e2d35d86e185a1723e5c
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-09 09:24:10 +0100

    Added the First Concrete Observer Implementation.

---
+++ Behavioral/Observer/Java Implementation/ObserverDesignPattern/src/main/java/ConcreteObserver/EmailNotificationListener.java
@@ -0,0 +1,18 @@
+package ConcreteObserver;
+
+import Observer.EventListener;
+
+import java.io.File;
+
+public class EmailNotificationListener implements EventListener {
+    private String email;
+
+    public EmailNotificationListener(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Email to " + email + ": Someone has performed " + eventType + " operation with the following file: " + file.getName());
+    }
+}
