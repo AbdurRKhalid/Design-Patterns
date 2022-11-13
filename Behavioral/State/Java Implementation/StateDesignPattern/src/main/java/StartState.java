2a7dc19495c4aca5c9c59f0b4727003b734c8aac
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-13 22:57:53 +0100

    Added the Implementation of the Start State.

---
+++ Behavioral/State/Java Implementation/StateDesignPattern/src/main/java/StartState.java
@@ -0,0 +1,13 @@
+import javax.naming.Context;
+
+public class StartState implements State{
+    @Override
+    public void doAction(Context context) {
+        System.out.println("Player is in start state");
+        context.setState(this);
+    }
+
+    public String toString(){
+        return "Stop State";
+    }
+}
