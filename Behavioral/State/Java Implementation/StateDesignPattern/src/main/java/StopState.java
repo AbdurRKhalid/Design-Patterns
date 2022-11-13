d12fece59ad0173e3353e5d859ea7bc67a2d5a5e
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-13 23:00:11 +0100

    Added the Test Code and Stop State Implementation.

---
+++ Behavioral/State/Java Implementation/StateDesignPattern/src/main/java/StopState.java
@@ -0,0 +1,10 @@
+public class StopState implements State{
+    public void doAction(Context context) {
+        System.out.println("Player is in stop state");
+        context.setState(this);
+    }
+
+    public String toString(){
+        return "Stop State";
+    }
+}
---
+++ Behavioral/State/Java Implementation/StateDesignPattern/src/main/java/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        StartState startState = new StartState();
+        startState.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+
+        System.out.println(context.getState().toString());
+    }
+}
