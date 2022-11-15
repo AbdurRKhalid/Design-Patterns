e7d19cd2ac32a1a8ea374da9a052ce4fad3e901b
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-15 10:48:31 +0100

    Added the Simple Implementation of Network Abstract Class.

---
+++ Behavioral/Template Method/Java Implementation/TemplateMethodDesignPattern/src/main/java/Network.java
@@ -0,0 +1,25 @@
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    /**
+     * Publish the data to whatever network.
+     */
+    public boolean post(String message) {
+        // Authenticate before posting. Every network uses a different
+        // authentication method.
+        if (logIn(this.userName, this.password)) {
+            // Send the post data.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
