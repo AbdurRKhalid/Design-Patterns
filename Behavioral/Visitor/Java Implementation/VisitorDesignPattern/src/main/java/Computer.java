310d4ff355a2573f97d3c7422a1ea9d5e7bef606
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2022-11-16 08:49:12 +0100

    Added the Implementation of the Computer Class for Visitor Design Pattern.

---
+++ Behavioral/Visitor/Java Implementation/VisitorDesignPattern/src/main/java/Computer.java
@@ -0,0 +1,16 @@
+public class Computer implements ComputerPart {
+    ComputerPart[] parts;
+
+    public Computer(){
+        parts = new ComputerPart[] {new Mouse(), new Keyboard(), new Monitor()};
+    }
+
+
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        for (int i = 0; i < parts.length; i++) {
+            parts[i].accept(computerPartVisitor);
+        }
+        computerPartVisitor.visit(this);
+    }
+}
